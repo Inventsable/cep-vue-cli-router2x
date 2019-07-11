1d2fdecc5fed212adbf91d2c824421526f5abada
Tom Scharstein <trscharstein@gmail.com>
2019-07-10 20:47:47 -0700

    Fixing missing utils

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,157 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <stylizer/>
+    <identity/>
+    <menus/>
+    <version/>
   </div>
 </template>
 
+<script>
+import identity from "./components/main/identity.vue";
+import stylizer from "./components/main/stylizer.vue";
+import menus from "./components/main/menus.vue";
+import version from "./components/main/version.vue";
+
+export default {
+  name: "App",
+  components: {
+    identity,
+    stylizer,
+    menus,
+    version
+  },
+  computed: {
+    storage() {
+      return window.localStorage;
+    }
+  },
+  data: () => ({
+    csInterface: null,
+    identity: null,
+    stylizer: null,
+    menus: null,
+    isMounted: false
+  }),
+  mounted() {
+    console.clear();
+    this.csInterface = new CSInterface();
+    this.csInterface.addEventListener("console", this.consoler);
+
+    // Utility components are already mounted prior to this
+    console.log(
+      `${this.identity.extName} ${this.identity.extVersion} : ${
+        this.identity.isDev ? "DEV" : "BUILD"
+      }`
+    );
+    this.isMounted = true;
+
+    this.loadUniversalScripts();
+
+    // Vue Router must be manually initialized in CEP:
+    this.$router.push({ name: "home" });
+  },
+  methods: {
+    dispatchEvent(name, data) {
+      var event = new CSEvent(name, "APPLICATION");
+      event.data = data;
+      this.csInterface.dispatchEvent(event);
+    },
+    loadScript(path) {
+      // Correctly loads a script regardless of whether Animate or regular CEP app
+      if (!/FLPR/.test(this.identity.appName))
+        this.csInterface.evalScript(`$.evalFile('${path}')`);
+      else
+        this.csInterface.evalScript(
+          `fl.runScript(FLfile.platformPathToURI("${path}"))`
+        );
+    },
+    loadUniversalScripts() {
+      // Preloads any script located inside ./src/host/universal
+      let utilFolder = window.cep.fs.readdir(
+        `${this.identity.root}/src/host/universal/`
+      );
+      if (!utilFolder.err) {
+        let valids = utilFolder.data.filter(file => {
+          return /\.(jsx|jsfl)$/.test(file);
+        });
+        valids.forEach(file => {
+          this.loadScript(`${this.identity.root}/src/host/universal/${file}`);
+        });
+      }
+      // Preloads any script located in ./src/host/[appName]/
+      let hostFolder = window.cep.fs.readdir(
+        `${this.identity.root}/src/host/${this.identity.appName}/`
+      );
+      if (!hostFolder.err) {
+        let valids = hostFolder.data.filter(file => {
+          return /\.(jsx|jsfl)$/.test(file);
+        });
+        valids.forEach(file => {
+          this.loadScript(
+            `${this.identity.root}/src/host/${this.identity.appName}/${file}`
+          );
+        });
+      } else {
+        console.log(
+          `${this.identity.root}/src/host/${
+            this.identity.appName
+          } has no valid files or does not exist`
+        );
+      }
+    },
+    consoler(msg) {
+      // Catches all console.log() usage in .jsx files via CSEvent
+      console.log(`${this.identity.appName}: ${msg.data}`);
+    },
+    getCSS(prop) {
+      // Returns current value of CSS variable
+      // prop == typeof String as name of variable, with or without leading dashes:
+      // this.getCSS('color-bg') || this.getCSS('--scrollbar-width')
+      return window
+        .getComputedStyle(document.documentElement)
+        .getPropertyValue(`${/^\-\-/.test(prop) ? prop : "--" + prop}`);
+    },
+    setCSS(prop, data) {
+      // Sets value of CSS variable
+      // prop == typeof String as name of variable, with or without leading dashes:
+      // this.setCSS('color-bg', 'rgba(25,25,25,1)') || this.setCSS('--scrollbar-width', '20px')
+      document.documentElement.style.setProperty(
+        `${/^\-\-/.test(prop) ? prop : "--" + prop}`,
+        data
+      );
+    }
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 #nav {
-  padding: 30px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  color: var(--color-text-default);
 }
 
 #nav a {
+  padding: 0px 5px;
   font-weight: bold;
-  color: #2c3e50;
+  color: var(--color-text-default);
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+}
+
+:root {
+  --toolbar-height: 40;
+  --quad: cubic-bezier(0.48, 0.04, 0.52, 0.96);
+  --quart: cubic-bezier(0.76, 0, 0.24, 1);
+  --quint: cubic-bezier(0.84, 0, 0.16, 1);
 }
 </style>
